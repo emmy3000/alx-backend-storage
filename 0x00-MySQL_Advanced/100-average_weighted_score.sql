ebf803c87ae92ae7d1848e58e7462caac319450a
Emeka Emodi <emodiemeka@gmail.com>
2023-10-13 03:38:52 +0000

    feat: Calculate and store weighted average score for a user.

---
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,19 @@
+-- Calculate and store the weighted average score for a student identified by user_id
+
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
+
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
+BEGIN
+    UPDATE users
+    SET average_score = (
+        SELECT SUM(corrections.score * projects.weight) / SUM(projects.weight)
+        FROM corrections
+        INNER JOIN projects ON projects.id = corrections.project_id
+        WHERE corrections.user_id = user_id
+    )
+    WHERE users.id = user_id;
+END$$
+
+DELIMITER ;
