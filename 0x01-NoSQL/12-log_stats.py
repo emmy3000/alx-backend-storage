fe4dc371fdcb4c44f8b2559d8eabcefccd1f32c3
Emeka Emodi <emodiemeka@gmail.com>
2023-10-18 05:09:22 +0000

    feat: Add 'log_stats' function to provide statistics about Nginx logs stored in MongoDB

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+"""
+Script provides statistics about Nginx logs stored in MongoDB
+
+Database: logs, Collection: nginx, Display results as shown in the example
+- First line: x logs, x number of documents in this collection
+- Second line: Methods
+- Five lines with method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+- One line with method=GET, path=/status
+"""
+from pymongo import MongoClient
+
+METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+
+def log_stats(mongo_collection, option=None):
+    """
+    Provide statistics about Nginx logs stored in MongoDB.
+
+    :param mongo_collection: The pymongo collection object.
+    :param option: (Optional) Filter logs by method.
+    """
+    if option:
+        value = mongo_collection.count_documents({"method": option})
+        print(f"\tmethod {option}: {value}")
+    else:
+        total_logs = mongo_collection.count_documents({})
+        print(f"{total_logs} logs")
+        print("Methods:")
+        for method in METHODS:
+            log_stats(mongo_collection, method)
+        status_check = mongo_collection.count_documents(
+            {"method": "GET", "path": "/status"})
+        print(f"method=GET, path=/status: {status_check}")
+
+
+if __name__ == "__main":
+    nginx_collection = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
+    log_stats(nginx_collection)
