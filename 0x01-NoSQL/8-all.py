484e91a9575750bfacb33ee2dd6a87d3344b28ce
Emeka Emodi <emodiemeka@gmail.com>
2023-10-18 04:41:53 +0000

    feat: Add 'list_all' function to retrieve all documents from a collection

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -7,7 +7,7 @@ Script lists all the documents in a collection
 def list_all(mongo_collection):
     """
     List all documents in a collection.
-    
+
     :param mongo_collection: The pymongo collection object.
     :return: A list of documents in the collection.
     """
